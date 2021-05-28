0bcb9a723d9bf105fae7ad2aedb762b876df9434
cloudboundboris <blevenzon@gmail.com>
2021-05-28 15:30:26 -0400

    finishing backend

---
+++ backend/api/GetResumeCounter.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;  
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+
+
+namespace Company.Function
+
+
+{
+    public static class GetResumeCounter
+    {
+        [FunctionName("GetResumeCounter")]
+        
+            public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, 
+            [CosmosDB(databaseName:"azureresumedb", collectionName: "counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counternum,
+            [CosmosDB(databaseName:"azureresumedb", collectionName: "counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter upcounternum,
+            ILogger log)
+        {
+            log.LogInformation("+++++ trigger function processed a request.");
+            
+            upcounternum = counternum;
+            upcounternum.Count +=1;
+
+            var jsonToReturn = JsonConvert.SerializeObject(counternum);
+              
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+               {
+                   Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+               };
+                
+        } 
+    }
+}
